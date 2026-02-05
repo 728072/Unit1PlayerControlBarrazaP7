fb802e78e5fb61c93d3d8890e484f7f9f25737de
728072 <728072@student.dusd.net>
2026-02-05 15:00:53 -0800

    done

--- P7PlayerControlBarraza/Assets/Scripts/Camera.cs
+++ P7PlayerControlBarraza/Assets/Scripts/Camera.cs
@@ -4,11 +4,8 @@ public class Camera : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
-
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-
-   }
+    }
 }
