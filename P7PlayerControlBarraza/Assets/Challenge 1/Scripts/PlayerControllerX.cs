1c8ac8af06d60faabf023d28f88801e2b53f95dc
728072 <728072@student.dusd.net>
2026-02-13 14:51:31 -0800

    done with challenge

--- P7PlayerControlBarraza/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ P7PlayerControlBarraza/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,30 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalInput;
+
+    public GameObject player;
+    public GameObject propeller;
+    public bool engineon;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (engineon == true)
+            {
+                engineon = false;
+            }
+            else engineon = true;
+        }
     }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.W))
-        transform.Translate(Vector3.up * speed);
+        if (engineon == true)
+            player.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        else return;
 
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+            if (engineon == true)
+                player.transform.Rotate(-Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+            else return;
+        if (Input.GetKey(KeyCode.DownArrow))
+            if (engineon == true)
+                player.transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * -verticalInput);
+            else return;
+        if (engineon)
+        {
+            propeller.transform.Rotate(Vector3.forward * rotationSpeed);
+        }
     }
 }
+
